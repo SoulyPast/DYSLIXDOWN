b0c9c3ed7149d40544a20fd35dfa498a48837a6c
SoulyPast <mssoulaimane@gmail.com>
2020-05-12 02:14:01 +0200

    primera par de usuari normal exercici de tipo 1

--- public/Script/Forma.js
+++ public/Script/Forma.js
@@ -1,17 +1,24 @@
+var attr="";
+var exe="";
 $(".nav-link").click(function(){
-    var exe = ($(this).text());
-    console.log(exe);
-    myFunction(exe);
+    $('#'+attr).removeClass( "active show" );
+    $("#"+attr+"-tab").removeAttr("disabled");
+    exe = ($(this).text());
+    console.log("hola" + exe);
+    console.log();
+    attr = $(this).attr('aria-controls');
+    $('#'+attr).addClass( "active show" );
+    $(".E"+exe).attr("disabled", true);
+    arraytext=[];
+    $("."+exe).unbind("click");
+    $("."+exe).click(function(){
+        arraytext.push($(this).text());
+        console.log($(this).text());
+        $("#"+exe).text(arraytext);
+        $(this).attr("disabled", true);
+        });
 
   });
-
-function myFunction(exe) {
-$("."+exe).click(function(){
-console.log($(this).text());
-$(this).attr("disabled", true);
-})}
-
-
 
 function shuffle(array) {
     var copy = [], n = array.length, i;
@@ -25,6 +32,7 @@ function shuffle(array) {
     }
     return copy;
   }
+  var keys = [];
 
 function getMessage() {
     var id_activitat= $("#id_activitat").val();
@@ -36,14 +44,15 @@ function getMessage() {
         var exercicis = JSON.parse(JSON.stringify(data.exercici));
         var keys = Object.keys(exercicis);
         for (var i = 0, len = keys.length; i < len; i++) {
-          console.log(exercicis[keys[i]]);
           var tag = document.createElement("div");
           tag.setAttribute("class", "tab-pane fade");
           tag.setAttribute("id", "pills-"+exercicis[keys[i]].id);
           tag.setAttribute("role", "tabpanel");
           tag.setAttribute("aria-labelledby", "pills-"+exercicis[keys[i]].id+"-tab");
+          tag.setAttribute("style", "text-center");
           var div = document.createElement("div");
-          div.setAttribute("class", "content border border-primary");
+          div.setAttribute("class", "content ");
+          div.setAttribute("style", "display: flex;align-items: center;justify-content: center;");
           var paraula= exercicis[keys[i]].resposta;
           cont=1;
           arraychar=[];
@@ -54,27 +63,55 @@ function getMessage() {
           }
           newarry=shuffle(arraychar);
           var div0 = document.createElement("div");
-          div0.setAttribute("class", "content border border-primary");
+          div0.setAttribute("class", "content");
 
           for (let x = 0; x < newarry.length; x++) {
             var div1 = document.createElement("button");
-            div1.setAttribute("class", "rounded-circle rounded-sm border border-danger d-flex p-2 justify-content-center btn Exercici"+(i+1));
-            div1.setAttribute("style", "height:50px;width:50px;");
+            div1.setAttribute("class", "rounded-circle mt-5 mb-5 ml-5 mr-5 text-center rounded-sm border border-primary d-flex p-2 justify-content-center btn Exercici"+exercicis[keys[i]].id);
+            div1.setAttribute("style", "height:50px;width:50px; display: block;");
             div1.setAttribute("type", "button");
             div.appendChild(div1);
             var text = document.createTextNode(newarry[x]);
             div1.appendChild(text);
           }
-
+          var reset = document.createElement("button");
+          reset.setAttribute("class", "btn btn-primary mt-2 mb-2 ml-5 resetExercici"+(i+1));
+          reset.setAttribute("type", "reset");
+          reset.setAttribute("value", "Reset");
+          var text1 = document.createTextNode('Reset');
+          reset.appendChild(text1);
+          var input = document.createElement("div");
+          input.setAttribute("class", "form-control resultat mt-2 ");
+          input.setAttribute("id", "Exercici"+(i+1));
+          input.setAttribute("readonly", "true");
           tag.appendChild(div);
+          tag.appendChild(input);
+          tag.appendChild(reset);
           modal = document.getElementById("pills-tabContent");
           modal.appendChild(tag);
+
         }
+        $("#Començar").click(function(){
+        $(".Començar").css("display", "none");
+        $("#pills-"+exercicis[keys[0]].id+"-tab").css("display", "block");
+        $('#pills-'+exercicis[keys[0]].id).addClass( "active show" );
+        $(".Exercici"+exercicis[keys[0]].id).unbind("click");
+        $(".Exercici"+exercicis[keys[0]].id).click(function(){
+            //arraytext.push($(this).text());
+            console.log($(this).text());
+            //$("#Exercici"+exercicis[keys[0]].id).text(arraytext);
+            $(this).attr("disabled", true);
+            });
+        });
     }
     });
  }
+
  getMessage();
 
-
-
-
+/*
+ $("#Començar").click(function(){
+    $("#pills-"+exercicis[keys[0]].id+"-tab").css("display", "block");
+    $('#pills-'+exercicis[keys[0]].id).addClass( "active show" );
+ });
+*/
--- resources/views/activitats1/show.blade.php
+++ resources/views/activitats1/show.blade.php
@@ -3,28 +3,23 @@
 <h1>{{$activitas->nom_activitat}} </h1>
 <h2 class = "{{$activitas->tipus->nom_tipus}}"> {{$activitas->tipus->nom_tipus}}</h2>
 <input id="id_activitat" type="number" value="{{$activitas->id}}" style="display:none">
+<div class="Començar wrap text-center justify-content" style="display:block;display: flex;align-items: center;justify-content: center;background-image: url('https://i.pinimg.com/originals/4a/96/e6/4a96e602750b8ef669a77565becf3939.gif');height:500px;">
+  <button class="button" id="Començar">Començar</button>
+</div>
+
 <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
 @php($count=0)
 @foreach($exercici as $key => $exerci)
 @php($count++)
   <li class="nav-item ml-2 mt-3">
-    <a class="nav-link" id="pills-{{$exerci->id}}-tab" data-toggle="pill" href="#pills-{{$exerci->id}}" role="tab" aria-controls="pills-{{$exerci->id}}" aria-selected="true" >Exercici{{$count}}</a>
+    <button class="nav-link EExercici{{$count}}" id="pills-{{$exerci->id}}-tab" data-toggle="pill" href="#pills-{{$exerci->id}}" role="tab" aria-controls="pills-{{$exerci->id}}" aria-selected="true" style="display:none" disabled>Exercici{{$count}}</button>
   </li>
 @endforeach
 </ul>
-<div class="tab-content" id="pills-tabContent">
-
-<div class="tab-pane fade" id="pills-18" role="tabpanel" aria-labelledby="pills-18-tab">
-<div class="content border border-primary">
-<button  class="rounded-circle rounded-sm border border-danger d-flex p-2 justify-content-center btn Exercici2" style="height:50px;width:50px;">D</button>
-<button type="button" class="rounded-circle rounded-sm border border-danger d-flex p-2 justify-content-center btn Exercici2" style="height:50px;width:50px;">E</button>
-<button type="button" class="rounded-circle rounded-sm border border-danger d-flex p-2 justify-content-center btn Exercici2" style="height:50px;width:50px;">U</button>
-<input class="btn btn-primary resetExercici2" type="reset" value="Reset">
-</div>
-</div>
-
+<div class="tab-content " id="pills-tabContent" style="background-image: url('https://i.pinimg.com/originals/4a/96/e6/4a96e602750b8ef669a77565becf3939.gif');">
 
 </div>
+
 @endsection
 @section('scripts')
 <script src="{{ asset('Script/Forma.js') }}" defer></script>
